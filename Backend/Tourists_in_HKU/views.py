e1c01a065b262657d7a5ed87664370853fa9fda7
LWtiffany <2577933967@qq.com>
2025-02-16 16:57:00 +0800

    Method Constraints

--- Backend/Tourists_in_HKU/views.py
+++ Backend/Tourists_in_HKU/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render
 from .forms import BookingForm
-from .models import Booking
+from .SendEmail import send_success_email  # 导入封装好的发送邮件方法
+
 
 def book_tour(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            form.save()  # 将表单数据保存到数据库
-            return redirect('success')  # 重定向到成功页面
-    else:
+            # 保存表单数据到数据库
+            form.save()
+
+            # 提取预定邮箱
+            email = form.cleaned_data['email']
+
+            # 调用封装的发送邮件方法
+            send_success_email(email)
+
+            # 返回成功预约的HTTP响应
+            return HttpResponse("Appointment successful! We have sent a confirmation email to your email.", status=200)
+    elif request.method == 'GET':
         form = BookingForm()
-    return render(request, 'Tourists_in_HKU/book_tour.html', {'form': form})
+        return render(request, 'Tourists_in_HKU/book_tour.html', {'form': form})
+    else:
+        return HttpResponse("Wrong method", status=405)  # 返回405方法不允许错误
+
 
 def success(request):
     return render(request, 'Tourists_in_HKU/success.html')
+
 
 def get_video(request):
     latitude = request.GET.get('latitude')
@@ -23,4 +38,3 @@ def get_video(request):
         return JsonResponse({'video_url': video.video_file.url})
     else:
         return JsonResponse({'error': 'Video not found'}, status=404)
-
